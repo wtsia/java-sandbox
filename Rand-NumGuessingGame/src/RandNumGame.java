a5611057261a84e33aaeff46dca2781558ca1346
Winston <winstontsia@gmail.com>
2022-10-29 04:10:37 -0700

    completed random number game

--- Rand-NumGuessingGame/src/RandNumGame.java
+++ Rand-NumGuessingGame/src/RandNumGame.java
@@ -6,20 +6,59 @@ public class RandNumGame {
 		Scanner input = new Scanner(System.in);
 		Random randGen = new Random();
 		
-		int[] inputArray = new int[2];
+		int[] inputArray = new int[3];
 		boolean validInput = false;
 		int num1;
 		int num2;
 		int absVal = 0;
+		int randNum;
+		int guessNum;
+		int guessRemaining;
+		int guessCounter = 0;
+		double guessOdds;
+		final int guessLimit = 5;
+		
 		
 		welcome();
 		
 		while (validInput == false) {
-			num1 = input.nextInt();
-			num2 = input.nextInt();
-			numCompare(num1, num2, absVal, inputArray, validInput);
+			if (inputArray[0] == 0 && inputArray[1] == 0) {
+				num1 = input.nextInt();
+				num2 = input.nextInt();
+				numCompare(num1, num2, inputArray);
+			} else {
+				absVal = inputArray[2];
+				validInput = true;
+			}
 		}
 		
+		randNum = randGen.nextInt(absVal) + inputArray[0];
+		guessOdds = 100 - oddsCalculator(guessLimit, absVal)*100;
+		System.out.println("*******************  Introduction  *********************");
+		System.out.printf("You are awarded %d guesses.\n", guessLimit);
+		System.out.printf("Assuming %d random unique guesses, your odds of getting it\n", guessLimit);
+		System.out.printf("right is: %.2f%%\n", guessOdds);
+		System.out.println("May the odds be ever in your favor.");
+		System.out.println("********************************************************");
+		System.out.printf("I am a number, between %d  and %d. What am I?\n> ", inputArray[0], inputArray[1]);
+		
+		while (validInput == true) {
+			guessNum = input.nextInt();
+			guessCounter++;
+			guessRemaining = guessLimit - guessCounter;
+			if (guessCounter == guessLimit) {
+				System.out.printf("Close! But no cigar. The answer was %d. Better luck next time!", randNum);
+			} else {
+				if (guessNum == randNum) {
+					System.out.println("You got it!");
+					System.exit(0);
+				} else if (guessNum > randNum) {
+					System.out.printf("Too high! You have %d guesses left. Try again.\n> ", guessRemaining);
+				} else {
+					System.out.printf("Too low! You have %d guesses left. Try again.\n> ", guessRemaining);
+				}
+			}
+		}
 	}
 	
 	public static void welcome() {
@@ -28,22 +67,27 @@ public class RandNumGame {
 		System.out.print("> ");
 	}
 	
-	public static int[] numCompare(int numin1, int numin2, int absVal, int[] array, boolean statusCheck) {
+	public static int[] numCompare(int numin1, int numin2, int[] array) {
 		if (numin1 > numin2) {
 			array[0] = numin2;
 			array[1] = numin1;
-			statusCheck = true;
-			absVal = array[1] - array[0];
-			System.out.print("numCompare works!");
+			array[2] = array[1] - array[0];
 		} else if (numin2 > numin1) {
 			array[0] = numin1;
 			array[1] = numin2;
-			statusCheck = true;
-			absVal = array[1] - array[0];
-			System.out.print("numCompare works!");
+			array[2] = array[1] - array[0];
 		} else {
 			System.out.print("This range is 0! You might as well guess your name! \nTry Again!\n> ");
 		}
 		return array;
+	}
+	
+	public static double oddsCalculator(double guesses, double range) {
+		double odds = (range-1)/range;
+		for (int i = 1; i < guesses; i++) {
+			range--;
+			odds = odds*((range - 1)/range);
+		}
+		return odds;
 	}
 }
