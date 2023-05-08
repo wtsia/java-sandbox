728d76b2eadac8bb28aaad37c9f4eaca036bf490
Winston <winstontsia@gmail.com>
2023-05-07 21:37:24 -0700

    client server lab

---
+++ J23/ClientServerLab/src/ClientLab.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class ClientLab extends JFrame implements ActionListener{
+	
+		public static void main(String[] args) {
+			
+			
+		}
+}
